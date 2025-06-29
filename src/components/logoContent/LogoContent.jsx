c86008f6b6dec5db199a19c129df208f8b8153de
CosmicCoder22 <yura0092@gmail.com>
2025-06-29 16:08:38 +0300

    added weekly schedule page

---
+++ src/components/logoContent/LogoContent.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './logoContent.css'
+
+function LogoContent() {
+  return (
+    <div className='LogoContentBody'>
+      <h1 className='logoContentText mainContentLetter'>pa</h1>
+      <h1 className='logoContentText'>ro</h1>
+      <h1 className='logoContentText'>de</h1>
+      <h1 className='logoContentText'>nt</h1>
+    </div>
+  )
+}
+
+export default LogoContent
