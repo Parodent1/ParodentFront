3f0c4e70dd6f72c515aa45c442f6891a589141b2
CosmicCoder22 <yura0092@gmail.com>
2025-07-19 09:54:16 +0300

    adde emoji to appointment component

--- src/components/appointments/Appointment.jsx
+++ src/components/appointments/Appointment.jsx
@@ -1,47 +1,27 @@
-import React, { useState } from 'react'
-import './appointment.css'
-
+import React, { useState } from "react";
+import "./appointment.css";
+import EmojiSelector from "../emojiSelector/EmojiSelector";
 
 function Appointment() {
-  // const [menuVisible, setMenuVisible] = useState(false);
-  // const [menuPosition, setMenuPosition] = useState({ x: 0, y: 0 });
-
-  // const handleContextMenu = (event) => {
-  //   event.preventDefault();
-  //   setMenuPosition({ x: event.pageX, y: event.pageY });
-  //   setMenuVisible(true);
-  // };
-
-  // const handleClickOutside = () => {
-  //   setMenuVisible(false);
-  // };
-
+  const handleEmojiChange = (emoji) => {
+    console.log("Обрана емодзі:", emoji);
+  };
   return (
-    <div
-      className="appointmentBody"
-      // onContextMenu={handleContextMenu}
-      // onClick={handleClickOutside}
-    >
+    <div className="appointmentBody">
       <div className="appointmentHeader">
-      <h1 className="clientName">Заяць Данило Ігорович</h1>
-      <h1 className="appointmentTime">11:23–12:39</h1>        
+        <h1 className="clientName">Заяць Данило Ігорович</h1>
+        <h1 className="appointmentTime">11:23–12:39</h1>
       </div>
-
-      <p className="complaints">Біль в 8 зубі</p>
-      <p className="coment">агресивний пацієнт </p>
-
-      {/* {menuVisible && (
-        <div
-          className="contextMenu"
-          style={{ top: menuPosition.y, left: menuPosition.x }}
-        >
-          <button>Редагувати</button>
-          <button>Перенести</button>
-          <button>Видалити</button>
-          <button>Історія</button>
-          <button>Закрити</button>
-        </div>
-      )} */}
+      <div className="appointmentsComplains">
+        <p className="complaints">Біль в 8 зубі</p>
+        <p className="coment">агресивний пацієнт </p>
+      </div>
+      <div className="appointmentsEmoji">
+        <EmojiSelector
+          emojiKey="doctorEmoji"
+          onEmojiChange={handleEmojiChange}
+        />
+      </div>
     </div>
   );
 }
